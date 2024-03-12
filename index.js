d8f74d93f03edd2bc2d594cdf386456e595afb34
evgueni <djiniuswayne@outlook.com>
2024-03-12 18:52:02 +0100

    version finale v12 :

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
 let app = new Vue({
-    el: '#app', 
-    data: {
-        produits: {
-            id : 1,
-            nom: 'Cravate',
-            modeles: [
-                {
-                    id: 1,
-                    couleur: 'bleue',
-                    description: "La cravate classique passe partout.",
-                    prix: 52,
-                    image: 'img/cravate_bleue.png',
-                    stock: 4
-                },
+    el: '#app',
+    data() {
+        return {
+            produits: {
+                id: 1,
+                nom: 'Cravate',
+                modeleSelectionne: 0,
+                modeles: [
+                    {
+                        id: 1,
+                        couleur: 'bleue',
+                        description: "La cravate classique passe partout.",
+                        prix: 52,
+                        image: 'img/cravate_bleue.png',
+                        stock: 4
+                    },
+                    {
+                        id: 2,
+                        couleur: 'bleue nuit',
+                        description: "Idéale pour accompagner vos plus beaux costumes.",
+                        prix: 55,
+                        image: 'img/cravate_bleuenuit.png',
+                        stock: 5
+                    },
+                    {
+                        id: 3,
+                        couleur: 'rouge',
+                        description: "Une cravate pour la personne qui aime se faire remarquer.",
+                        prix: 51,
+                        image: 'img/cravate_rouge.png',
+                        stock: 6
+                    },
+                    {
+                        id: 4,
+                        couleur: 'turquoise',
+                        description: "Pour la personne qui a de l'audace.",
+                        prix: 49,
+                        image: 'img/cravate_verte.png',
+                        stock: 4
+                    },
+                ]
+            },
 
-                {
-                    id: 2,
-                    couleur: 'bleue nuit',
-                    description: "Idéale pour accompagner vos plus beaux costumes.",
-                    prix: 55,
-                    image: 'img/cravate_bleuenuit.png',
-                    stock: 5
-                },
-
-                {
-                    id: 3,
-                    couleur: 'rouge',
-                    description: "Une cravate pour la personne qui aime se faire remarquer.",
-                    prix: 51,
-                    image: 'img/cravate_rouge.png',
-                    stock: 6
-                },
-
-                {
-                    id: 4,
-                    couleur: 'turquoise',
-                    description: "Pour la personne qui a de l'audace.",
-                    prix: 49,
-                    image: 'img/cravate_verte.png',
-                    stock: 4
-                },
-            ],
-            modeleSelectionne: 0
-        },
-        panier: {
-            articles: [],
-            total: 0
+            panier: {
+                articles: [],
+                total: 0
+            }
         }
     },
+
     computed: {
         varianteSelectionnee() {
             return this.produits.modeles[this.produits.modeleSelectionne];
@@ -56,6 +57,7 @@ let app = new Vue({
             return this.panier.total >= 300;
         }
     },
+
     methods: {
         ajouterAuPanier() {
             let variante = this.varianteSelectionnee;
@@ -70,9 +72,11 @@ let app = new Vue({
                 variante.stock--;
             }
         },
+
         mettreAJourTotalPanier() {
             this.panier.total = this.panier.articles.reduce((total, article) => total + (article.prix * article.quantite), 0);
         },
+        
         viderPanier() {
             this.panier.articles.forEach(article => {
                 let variante = this.produits.modeles.find(variante => variante.id === article.id);
